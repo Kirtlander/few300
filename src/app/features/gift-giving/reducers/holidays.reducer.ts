b4816d7e30c814d2dd0a784657da7258bcfddff2
Kirtlander <19224678+Kirtlander@users.noreply.github.com>
2019-12-05 10:39:21 -0500

    add back in commented initialState mock for future

--- src/app/features/gift-giving/reducers/holidays.reducer.ts
+++ src/app/features/gift-giving/reducers/holidays.reducer.ts
@@ -16,6 +16,14 @@ export const adapter = createEntityAdapter<HolidayEntity>();
 
 const initialState = adapter.getInitialState();
 
+// const initialState: HolidayState = {
+//   ids: ['1', '2'],
+//   entities: {
+//     1: { id: '1', name: 'Christmas', date: new Date(2019, 11, 25).toISOString() },
+//     2: { id: '2', name: 'Jeff\'s Birthday', date: new Date(2020, 3, 20).toISOString() }
+//   }
+// };
+
 const reducerFunction = createReducer(
   initialState,
   on(actions.addHoliday, (state, action) => adapter.addOne(action.payload, state)),
